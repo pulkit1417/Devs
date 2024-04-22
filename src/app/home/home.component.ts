28c3e5fec6f82f6b1d9531338326abb61cf679be
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-04-22 21:18:18 +0530

    hosted and home page changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TeamComponent } from '../team/team.component';
-import { ServicesComponent } from '../services/services.component';
-import { ReviewsComponent } from '../reviews/reviews.component';
-import { WorksComponent } from '../works/works.component';
-import { ContactComponent } from '../contact/contact.component';
-import { HeaderComponent } from '../header/header.component';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgxTypedJsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+      
 }
